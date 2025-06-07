51a73c3cb3acfa9b82a59623342458d4f3b6b7f8
Damir <nazmidenovdamir@gmail.com>
2025-06-07 11:33:45 +0500

    third commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let msg1 = 'Hello world';
+let msg2 = 'Goodbye world';
+
+let elem1 = document.getElementById('one');
+elem1.textContent = msg1;
+
+let elem2 = document.getElementById('two');
+elem2.textContent = msg2
